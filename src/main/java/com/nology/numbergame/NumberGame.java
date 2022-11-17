a97208c54c4cb9d82c1ddf2f4c02d2273dc1e0d6
Anusha Antony <anushaca1995@gmail.com>
2022-11-17 14:13:47 +0000

    Final changes made

--- src/main/java/com/nology/numbergame/NumberGame.java
+++ src/main/java/com/nology/numbergame/NumberGame.java
@@ -11,65 +11,70 @@ public class NumberGame {
         Random rand = new Random();
         int randomNum = rand.nextInt(101);
         //System.out.println(attempt + " and given=" + number);
-        if (5 == number) {
+        if (randomNum == number) {
             if (attempt == 1) {
-                winMessage = "Congrats "+userName.toUpperCase()+", you won!"+"\nYour number of attempts = " + attempt;
+                winMessage = "Congrats "+userName.toUpperCase()+", You Won!"+"\nNumber Of Attempts = " + attempt;
                 flag=false;
             } else if (attempt < 3) {
-                winMessage = "Very very warm";
+                winMessage = "Very Very Warm";
             } else if (attempt < 6) {
-                winMessage = "Very warm";
+                winMessage = "Very Warm";
             } else if (attempt < 10) {
                 winMessage = "Warm";
             } else if (attempt < 16) {
                 winMessage = "Cold";
             } else if (attempt < 21) {
-                winMessage = "Very cold";
+                winMessage = "Very Cold";
             } else if (attempt < 25) {
                 winMessage = "Freezing";
             } else if (attempt < 30) {
                 winMessage = "Artic";
             }
             if (flag){
-                winMessage = winMessage +" "+userName.toUpperCase()+"\nYour number of attempts = " + attempt;
+                winMessage = winMessage +" "+userName.toUpperCase()+"\nYou Won This Game On " + attempt+" Attempts";
             }
-            System.out.println(winMessage);
+            System.out.println("----------------------------------------\n\n"+winMessage+"\n\n----------------------------------------\n");
             return true;
         } else if (attempt == 10) {
             System.out.println("No anywhere.\n You exceeded number of attempts \n Better luck next time "+userName);
         } else{
-            System.out.println("Your ");
+            System.out.println("Sorry! Mismatching..Please Try Again ");
         }
         return false;
     }
 
-    public static void main(String[] args) {
+    public void setPlay(String userName) {
         int attempt = 0;
+        do {
+            Scanner myObj = new Scanner(System.in);
+            System.out.println("Enter A Number Between 1 And 100");
+            String number = myObj.nextLine();
+            int num;
+            try {
+                num = Integer.parseInt(number);
+                attempt++;
+                NumberGame obj = new NumberGame();
+                if (obj.play(num, attempt, userName)) {
+                    return;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Not a valid number");
+                attempt++;
+            }
+        } while (attempt < 10);
+    }
+
+    public static void main(String[] args) {
         Scanner scObj = new Scanner(System.in);
         System.out.println("Welcome to Number Game\n\nPlease Enter Your Name");
         String userName = scObj.nextLine();
-        System.out.println("Hi "+userName+", Are you ready to start your game? \nPlease ENTER to start or type NO for exit");
+        System.out.println("Hi "+userName.toUpperCase()+", Are you ready to start your game? \nPlease ENTER to start or type NO for exit");
         String gameStart = scObj.nextLine();
         if (gameStart.toLowerCase()=="no") {
             System.out.println("Exit Game");
         }else{
-            do {
-                Scanner myObj = new Scanner(System.in);
-                System.out.println("Enter a number between 1 and 100");
-                String number = myObj.nextLine();
-                int num;
-                try {
-                    num = Integer.parseInt(number);
-                    attempt++;
-                    NumberGame obj = new NumberGame();
-                    if (obj.play(num, attempt, userName)) {
-                        return;
-                    }
-                } catch (NumberFormatException e) {
-                    System.out.println("Not a valid number");
-                    attempt++;
-                }
-            } while (attempt < 10);
+            NumberGame obj = new NumberGame();
+            obj.setPlay(userName);
         }
 
     }
